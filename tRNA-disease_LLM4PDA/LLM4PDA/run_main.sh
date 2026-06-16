8bf12a2e0ce2b814b91807e6f2054dfbd974c859
Xinagkui <49864808+xiangkui9e@users.noreply.github.com>
2026-06-16 16:06:50 +0800

    Update run_main.sh

--- tRNA-disease_LLM4PDA/LLM4PDA/run_main.sh
+++ tRNA-disease_LLM4PDA/LLM4PDA/run_main.sh
@@ -2,10 +2,10 @@ chmod +x main.py
 for i in {1..50}
 do
     folder_name="folder_$i"
-    data_name="data_$i"  # 取消赋值时的空格
+    data_name="data_$i"
     mkdir -p $folder_name
     
-    # 传入相对路径给 Python 脚本
+
     python main.py "../data/$data_name/fold_info.pickle"
     
     mv *.xlsx "$folder_name"
