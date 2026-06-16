cdbaef0729335567e42a7827fe10ee46e3bee984
Xinagkui <49864808+xiangkui9e@users.noreply.github.com>
2026-06-16 16:20:47 +0800

    Update dis_emb_to_pkl.py

--- feat/dis_emb_to_pkl.py
+++ feat/dis_emb_to_pkl.py
@@ -1,11 +1,11 @@
 import ast
 import pandas as pd
 
-dis_emb = '02-疾病嵌入生成-small/gpt-5.4_dis_emb.txt'
+dis_emb = '02-small/gpt-5.4_dis_emb.txt'
 data = pd.read_csv(dis_emb, sep=r':{4,}', header=0, engine='python')
 all_data = data.drop_duplicates(subset=['Disease'])
 final_data = all_data[['Disease', 'Embedding']]
 
 # Apply the funcption to get embeddings and create a dictionary mapping 'Disease' to embeddings
 disease_emb_dict = {dis: emb for dis, emb in zip(final_data['Disease'], final_data['Embedding'].apply(ast.literal_eval))}
-pd.to_pickle(disease_emb_dict, '02-疾病嵌入生成-small/gpt-5.4_dis_emb.pkl')
+pd.to_pickle(disease_emb_dict, '02-small/gpt-5.4_dis_emb.pkl')
