1d78de617200bd277eda404a609006f09408f2c2
Xinagkui <49864808+xiangkui9e@users.noreply.github.com>
2026-06-16 16:07:15 +0800

    Update run_result_compare.sh

--- tRNA-disease_LLM4PDA/LLM4PDA/run_result_compare.sh
+++ tRNA-disease_LLM4PDA/LLM4PDA/run_result_compare.sh
@@ -3,7 +3,7 @@ for i in {1..24}
 do
     folder_name="result"
     mkdir -p $folder_name
-	# 传入计算结果给 Python 脚本
-    python result_compare.py $((i*5-5))  # 执行数学计算并传入 Python 脚本
+
+    python result_compare.py $((i*5-5))
 	mv *.xlsx "$folder_name"
 done
